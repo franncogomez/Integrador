6198f5807319ee3e8398838af469225828fd071f
franncogomez <fgomez834@gmail.com>
2022-06-13 17:58:04 -0300

    Actualización de Package, todavía sigue sin funcionar

--- pkg/avatar/encoder/encoder.go
+++ pkg/avatar/encoder/encoder.go
@@ -4,7 +4,7 @@ import "crypto/md5"
 
 type MD5Encoder struct{}
 
-func (e *MD5Encoder) EncodeInformation (name string) (encodeInformation []bytes, err error){
-	hash := md5.Sum([]bytes(name))
-	return hash[:], err nil
+func (e *MD5Encoder) Encodeinformation (name string) (encodeInformation []byte, err error){
+	hash := md5.Sum([]byte (name))
+	return hash[:], err
 }
